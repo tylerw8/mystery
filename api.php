03b34f0b4d80a60831a2bb5ef7697607877a6f72
Tyler Waite <waite.tyler@gmail.com>
2015-02-15 15:19:59 -0800

    Updated api and index files

--- api.php
+++ api.php
@@ -28,7 +28,7 @@ if(isset($_REQUEST['cat']))
 {//check to be sure data has been transmitted via GET or POST
 	switch($_REQUEST['cat'])
 	{//determine contents of 'cat'
-		case "rate":
+		case "rating":
 			include('data/ms3000ratings.js'); //"rate" orders by ratings
 			break;
 		default:
